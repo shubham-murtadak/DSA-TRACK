63e10a2f377dbbad69d5448e58dbaf3956c5a7a4
Shubham Murtadak <125735421+shubham-murtadak@users.noreply.github.com>
2023-05-08 10:48:27 +0530

    swap chaged

--- 3.Arrays Time & Space Complexicity/1.Lecture/Homework/1implimentswapfunction.cpp
+++ 3.Arrays Time & Space Complexicity/1.Lecture/Homework/1implimentswapfunction.cpp
@@ -16,21 +16,11 @@ void swap2(int a, int b){
 }
 
 void swap3(int a, int b){
-    int x = a ^ b;
-    int i=0;
-    while(x){
-        int curr = 1<<i;
-        // if that bit in x is 1, then that bit is different in a and b
-        if(x & curr ){
-            // change that bit in a
-            a = a ^ curr;
-            // change that bit in b
-            b = b ^ curr;
-            // make that 1 bit to 0 in x
-            x = x ^ curr;
-        }
-        i++;
-    }
+     a=a^b;
+     b=a^b;
+     a=a^b;
+        
+   
     cout<<a<<' '<<b<<'\n';
 }
 
